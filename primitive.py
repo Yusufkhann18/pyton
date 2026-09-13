2b3ad8f2f7d7d8c9e2c5279b02262f6ef64551e4
soliboyev muhammadyusuf <soliboyev007@soliboyevs-MacBook-Air.local>
2026-09-13 19:24:13 +0900

    feat: learn string in py

--- primitive.py
+++ primitive.py
@@ -8,3 +8,20 @@ print(f"the count: {count}, type: {count_type}")
 result1 = count.bit_count()
 result2 = count.numerator
 print(f"bit count: {result1}, numerator: {result2}")
+
+print("===============string=================")
+# Methods: upper(), lower(), title(), capitalize(), isupper(), islower(), istitle(), isalpha(), isdigit(), isspace()
+
+course = " Ai Python full stack"
+result = type(course)
+print(f"the course: {course}, type: {result}")
+result = course.title()
+print(f"the course: {course}, type: {result}")
+result = course.upper()
+print(f"the course: {course}, type: {result}")
+result = course.lower()
+print(f"the course: {course}, type: {result}")
+result = course.capitalize()
+print(f"the course: {course}, type: {result}")
+result = course.replace("Python", "Java")
+print(f"the course: {course}, type: {result}")
